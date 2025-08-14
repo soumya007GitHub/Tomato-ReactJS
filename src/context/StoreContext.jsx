94a2e1085922cfe81ac4ac2e3c561a8f91a35ca5
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-14 19:04:29 +0530

    fixed storecontext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -16,7 +16,7 @@ const StoreContextProvider = (props) => {
     }
 
     const removeFromCart = (itemId) => {
-        setCartItems(prev => ({ ...prev, [itemId]: prev[itemId - 1] }))
+        setCartItems(prev => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
     const getTotalCartAmount = () => {
